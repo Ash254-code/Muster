c7ba63884e17949d9decfca64fff1a31734791c6
Ash254-code <ash@greenwoodpark.com.au>
2026-04-11 21:47:35 +0930

    Lower bottom panel closer to screen edge

--- Muster/Views/Muster/MapMainView.swift
+++ Muster/Views/Muster/MapMainView.swift
@@ -3434,7 +3434,7 @@ private func previewThumbnail(for option: MapModeOption) -> some View {
         )
         .shadow(color: chromeShadow, radius: 18, y: 4)
         .padding(.horizontal, 6)
-        .padding(.bottom, 2)
+        .padding(.bottom, 2 - safeAreaBottom)
         .contentShape(Rectangle())
         .gesture(bottomPanelDragGesture)
     }
